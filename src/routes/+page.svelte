f44258c11ad9d360de5b11297483c9550858e191
yosshipaopao <yosshipaopao@gmail.com>
2023-10-03 20:15:53 +0900

    add Home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,10 +4,13 @@
 </svelte:head>
 
 <div class='hero min-h-screen fixed top-0 -z-10'>
-    <div class='hero-content text-center'>
-        <div class='space-y-4'>
-            <h1 class='text-5xl font-bold'>ホームもまだ作ってない</h1>
-            <p class='text-2xl'>まだ作ってないよ～～～ {"><;"}</p>
+    <div class='hero-content flex md:flex-row flex-col'>
+        <div class="w-1/3">
+            <img src="/logo.png" alt="豆腐創作活動研究会" class="w-full">
+        </div>
+        <div class="w-2/3">
+            <div class="text-6xl font-bold text-white">豆腐創作活動研究会</div>
+            <div class="text-2xl font-bold text-white">文化祭用特設サイト</div>
         </div>
     </div>
 </div>
