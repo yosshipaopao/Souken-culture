ca15c3c32af605cff7ab7a3139cf5276aca922f7
yosshipaopao <yosshipaopao@gmail.com>
2023-10-08 19:48:56 +0900

    ランキング削除

--- src/routes/result/[course]/+page.svelte
+++ src/routes/result/[course]/+page.svelte
@@ -36,7 +36,7 @@
 <div class='hero min-h-screen fixed top-0 -z-10'>
     <div class='hero-content text-center'>
         <div class='flex flex-col md:flex-row gap-4'>
-            <div class='h-96 flex flex-col items-center justify-end md:items-end md:justify-center gap-2'>
+            <div class='h-96'>
                 {#if data.yourResult}
                     <h1 class='font-bold text-3xl'>Result</h1>
                     <h1 class='font-bold text-3xl'>start:   {dateToTimeString(data.yourResult.start)}</h1>
@@ -45,17 +45,6 @@
                 {:else}
                     <h1 class='font-bold text-3xl'>未達成</h1>
                 {/if}
-            </div>
-            <div class='h-96 flex flex-col items-center justify-start md:items-start md:justify-center gap-2'>
-                {#each data.fastest as user}
-                    <div class="card card-side h-24 bg-base-200 shadow-xl">
-                        <figure><img src={user.image} alt="icon"/></figure>
-                        <div class="card-body py-3 text-left">
-                            <h2 class="card-title">{user.name}</h2>
-                            <p>total:   {calTotal(user.total)}</p>
-                        </div>
-                    </div>
-                {/each}
             </div>
         </div>
     </div>
